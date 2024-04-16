13ac059fd7d66dc1526a3d3c789bebf8c169948b
JorgeLopez3092 <jorgelopez3092@gmail.com>
2024-04-15 19:29:23 -0700

    template folder and structure update

--- app.py
+++ app.py
@@ -7,6 +7,28 @@ from models.database import db, app
 
 @app.route('/')
 def index() -> str:
+    print(app.template_folder)
+    # return 'hello world!'
+    return render_template('index.html')
+
+
+@app.route('/projects/new')
+def project_create() -> str:
+    return 'Hello World!'
+
+
+@app.route('/projects/<id>')
+def project_details() -> str:
+    return 'Hello World!'
+
+
+@app.route('/projects/<id>/edit')
+def project_edit() -> str:
+    return 'Hello World!'
+
+
+@app.route('/projects/<id>/delete')
+def project_delete() -> str:
     return 'Hello World!'
 
 
